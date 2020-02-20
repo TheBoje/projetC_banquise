25815036c0aa51f277f1b0b1c099a0fc53f488f8
inesmes-1 <61236143+inesmes-1@users.noreply.github.com>
2020-02-20 10:40:45 +0100

    fonctions

--- main.c
+++ main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+T_case **alloue(int n)
+{
+    int h;
+    T_case ** tab =(T_case**) malloc(n*sizeof(T_case*));
+
+    for (h = 0 ; h < n ; h++)
+    {
+        tab[h] = (T_case*)malloc(n * sizeof(T_case));
+    }
+    return tab;
+
+    }
+
+void remp_banquise_tab(T_case **tab, int n){
+   int i,j;
+   srand(time(NULL));
+    for(i=0; i<n;i++){
+        for (j=0;j<n;j++){
+       tab[i][j]->type_case = 1;
+        }
+    }
+
+}
+
+void  affiche_banquise (T_case **tab, int n){
+int i,j;
+ for(i=0; i<n;i++){
+        for (j=0;j<n;j++){
+          fprintf(stdout, " %d " ,tab[i][j]->type_case);
+        }
+        fprintf(stdout,"\n");
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 int main()
 {
     printf("Hello world!\n");
